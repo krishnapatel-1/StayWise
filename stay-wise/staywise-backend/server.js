16e42aab474dd7f08f7a3c4166b884d99075ec49
ajayrajput8 <singhrajputajay2004@gmail.com>
2025-07-14 23:46:20 +0530

    made searchbar searchable

--- stay-wise/staywise-backend/server.js
+++ stay-wise/staywise-backend/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 
 import userRoutes from "./routes/userRoutes.js";
 import propertyRoutes from "./routes/propertyRoutes.js";
+import locationRoutes from './routes/locationRoutes.js';
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
+/*app.use(cors());*/
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
@@ -35,6 +37,7 @@ mongoose.connect(process.env.MONGO_URI, {
   // Routes
   app.use("/api/users", userRoutes);
   app.use("/api/properties", propertyRoutes);
+  app.use('/api', locationRoutes);
 
   // Start server
   app.listen(PORT, "0.0.0.0", () => {
